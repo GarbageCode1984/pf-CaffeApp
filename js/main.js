08b21f69118e81726db248bfbfbe2446bb389e5f
GarbaGeCode1984 <rkqlwl1984@gmail.com>
2022-11-24 18:18:34 +0900

    1

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let mainImage = document.querySelector(".slider")
+let v = 0;
+
+setInterval(() => {
+  if(v === 0 ){
+  document.querySelector(".slider").style.transform = 'translateX(-100%)';
+  v = 1;
+  } else if(v===1){
+  document.querySelector(".slider").style.transform = 'translateX(-200%)';
+  v = 2;
+  } else if(v===2){
+  document.querySelector(".slider").style.transform = 'translateX(0)';
+  v = 0;
+  }
+}, 3000);
