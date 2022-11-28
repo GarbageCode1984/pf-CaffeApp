cce20ce35a090620f18108d2b11136b2832e8cd5
GarbageCode <75023330+GarbageCode1984@users.noreply.github.com>
2022-11-28 21:41:20 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-let mainImage = document.querySelector(".slider")
+let mainImage = document.querySelector(".slider");
 let v = 0;
 
 setInterval(() => {
-  if(v === 0 ){
-  document.querySelector(".slider").style.transform = 'translateX(-100%)';
-  v = 1;
-  } else if(v===1){
-  document.querySelector(".slider").style.transform = 'translateX(-200%)';
-  v = 2;
-  } else if(v===2){
-  document.querySelector(".slider").style.transform = 'translateX(0)';
-  v = 0;
-  }
+    if (v === 0) {
+        document.querySelector(".slider").style.transform = "translateX(-100%)";
+        v = 1;
+    } else if (v === 1) {
+        document.querySelector(".slider").style.transform = "translateX(-200%)";
+        v = 2;
+    } else if (v === 2) {
+        document.querySelector(".slider").style.transform = "translateX(0)";
+        v = 0;
+    }
 }, 3000);
 
 //nav
-let mainNav = document.querySelector('.navigate .head img')
+let mainNav = document.querySelector(".navigate .head img");
