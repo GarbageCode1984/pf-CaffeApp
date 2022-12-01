ae9c8bbcf084b05bc791126a8b03f2e17739b1d0
GarbageCode <75023330+GarbageCode1984@users.noreply.github.com>
2022-12-01 20:58:02 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,16 +2,16 @@ let mainImage = document.querySelector(".slider");
 let v = 0;
 
 setInterval(() => {
-    if (v === 0) {
-        document.querySelector(".slider").style.transform = "translateX(-100%)";
-        v = 1;
-    } else if (v === 1) {
-        document.querySelector(".slider").style.transform = "translateX(-200%)";
-        v = 2;
-    } else if (v === 2) {
-        document.querySelector(".slider").style.transform = "translateX(0)";
-        v = 0;
-    }
+  if (v === 0) {
+    document.querySelector(".slider").style.transform = "translateX(-100%)";
+    v = 1;
+  } else if (v === 1) {
+    document.querySelector(".slider").style.transform = "translateX(-200%)";
+    v = 2;
+  } else if (v === 2) {
+    document.querySelector(".slider").style.transform = "translateX(0)";
+    v = 0;
+  }
 }, 3000);
 
 //nav Change
@@ -20,11 +20,9 @@ let NavClose = document.querySelector(".navigate .x_icon");
 let NavChange = document.querySelector("nav");
 
 NavOpen.addEventListener("click", function () {
-    NavChange.classList.add("on");
+  NavChange.classList.add("on");
 });
 
 NavClose.addEventListener("click", function () {
-    NavChange.classList.remove("on");
+  NavChange.classList.remove("on");
 });
-
-//profile
